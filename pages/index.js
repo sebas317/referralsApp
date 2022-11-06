10f60bc9e17066585efc730d394fda7d759e6d17
YeissonOrtiz <ortizyeison2183@gmail.com>
2022-11-05 19:15:13 -0500

    Start of the frontend

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <h1 className="text-5xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <form className="flex flex-col w-2/4 mx-auto h-screen border-2">
+      <h1>test</h1>
+    </form>
+  );
 }
